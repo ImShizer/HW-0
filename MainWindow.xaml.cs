2e732a87f741d70b827ad9404b66afc9f32743fa
ImShizer <68175695+ImShizer@users.noreply.github.com>
2023-12-18 01:07:06 +0200

    Divan

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace HW_0
+{
+    public partial class MainWindow : Window
+    {
+        private int clickCount = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            clickCount++;
+            MessageBox.Show($"Количество нажатий: {clickCount}", "Сообщение");
+        }
+    }
+}
